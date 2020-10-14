af1693b05ba7dedc6c81a8ae3d29b1661a4c6093
Reelfan <ufuk.a@mail.de>
2020-10-14 16:46:14 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"valloxrs485 adapter settings": {
+		"en": "Adapter settings for valloxrs485",
+		"de": "Adaptereinstellungen für valloxrs485",
+		"ru": "Настройки адаптера для valloxrs485",
+		"pt": "Configurações do adaptador para valloxrs485",
+		"nl": "Adapterinstellingen voor valloxrs485",
+		"fr": "Paramètres d'adaptateur pour valloxrs485",
+		"it": "Impostazioni dell'adattatore per valloxrs485",
+		"es": "Ajustes del adaptador para valloxrs485",
+		"pl": "Ustawienia adaptera dla valloxrs485",
+		"zh-cn": "valloxrs485的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
